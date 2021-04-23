7489eb6c387afa49cea1e98e49fb146d0791af95
alessiogelso95 <80513491+alessiogelso95@users.noreply.github.com>
2021-04-23 11:00:42 +0200

    Create R_code_multivariate_analysis.r

---
+++ R_code_multivariate_analysis.r
@@ -0,0 +1,14 @@
+# R_code_multivariate_analysis.r
+
+#compatta i dati
+#componente principale 1 (range dei dati è il 90%)e la componente principale 2 (range è del 10%) è perpendicolare alla principale 1
+# analisi multivariata uso solo la PC1 perchè contiene il 90% dei dati
+
+setwd("C:/lab/)
+library(raster)
+library(RStoolbox)
+logan <- brick("p224r63_2011_masked.grd")
+
+plot(logan$B1_sre, logan$B2_sre, col="red", pch=19, cex=2) #pch=simboli
+pairs(p224r63_2011) #mette in correlazione le variabili a due a due, le variabili sono le nostre bande(0.93=correlatissime) indice di Chrinson. Quanto queste bande sono molto correlate
+# sistema di sette bande che 
